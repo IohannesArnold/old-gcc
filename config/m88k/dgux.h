01437051cb39673349dd179c811b99df25a1bd1a
tege <>
1996-10-08 23:39:12 +0000

    (CPP_SPEC): Avoid newline in the string.

--- config/m88k/dgux.h
+++ config/m88k/dgux.h
@@ -83,8 +83,7 @@ Boston, MA 02111-1307, USA.  */
    -traditional, or restricting include files to one specific source
    target, specify full DG/UX features.  */
 #undef	CPP_SPEC
-#define	CPP_SPEC "%(cpp_cpu) 
-		  %{msvr3:-D_M88KBCS_TARGET} %{!msvr3:-D_DGUX_TARGET}"
+#define	CPP_SPEC "%(cpp_cpu) %{msvr3:-D_M88KBCS_TARGET} %{!msvr3:-D_DGUX_TARGET}"
 
 /* Assembler support (-V, silicon filter, legends for mxdb).  */
 #undef	ASM_SPEC
